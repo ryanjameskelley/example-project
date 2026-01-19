7a75d04982a0b838e5f6727f38b55dd1c03cbca4
ryanjameskelley <ryanj.kelley@icloud.com>
2026-01-18 21:55:30 -0800

    Create apps/prototypes/registry.ts

---
+++ apps/prototypes/registry.ts
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import p0 from './1768709247.022939/index.tsx';
+import p1 from './1768710036.803279/index.tsx';
+import p2 from './1768713701.235959/index.tsx';
+import p3 from './1768716062.770699/index.tsx';
+import p4 from './1768717322.296009/index.tsx';
+import p5 from './1768717689.029299/index.tsx';
+import p6 from './1768718135.236779/index.tsx';
+import p7 from './1768719077.248559/index.tsx';
+import p8 from './1768719306.686919/index.tsx';
+import p9 from './1768791080.716189/index.tsx';
+import p10 from './1768791443.948999/index.tsx';
+import p11 from './1768793986.728619/index.tsx';
+import p12 from './1768801805.656949/index.tsx';
+import p13 from './1768802109.256559/index.tsx';
+
+export const prototypes: Record<string, React.ComponentType> = {
+  '1768709247.022939': p0,
+  '1768710036.803279': p1,
+  '1768713701.235959': p2,
+  '1768716062.770699': p3,
+  '1768717322.296009': p4,
+  '1768717689.029299': p5,
+  '1768718135.236779': p6,
+  '1768719077.248559': p7,
+  '1768719306.686919': p8,
+  '1768791080.716189': p9,
+  '1768791443.948999': p10,
+  '1768793986.728619': p11,
+  '1768801805.656949': p12,
+  '1768802109.256559': p13,
+};
