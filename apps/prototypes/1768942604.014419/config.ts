9893f958608c885a62e9e38a4ebeef97c2b7869f
ryanjameskelley <ryanj.kelley@icloud.com>
2026-01-20 22:42:08 -0800

    Add prototype files: apps/prototypes/1768942604.014419/index.tsx, apps/prototypes/1768942604.014419/config.ts, apps/prototypes/registry.ts, apps/components/AuuiBanner.tsx, src/App.tsx, vercel.json

--- apps/prototypes/1768942604.014419/config.ts
+++ apps/prototypes/1768942604.014419/config.ts
@@ -7,7 +7,7 @@ export interface PrototypeConfig {
 }
 
 const config: PrototypeConfig = {
-  id: '1768977321179',
+  id: '1768977727373',
   title: 'Can You Replace The Admin,',
   description: 'can you replace the admin, member, manager badges with the atom badge from @/components/atoms/badge',
   createdAt: '2026-01-21',
