2afdc90cbc6e8e9921a7452b791576e380c48bce
ryanjameskelley <ryanj.kelley@icloud.com>
2026-02-05 14:58:04 -0800

    Delete apps directory

--- apps/prototypes/registry.ts
+++
@@ -1,5 +0,0 @@
-// Auto-generated prototype registry
-// This file is updated automatically when prototypes are created
-export const prototypes = {
-  '1768942604.014419': () => import('./1768942604.014419'),
-};
